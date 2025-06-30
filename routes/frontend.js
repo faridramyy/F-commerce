7f715ffbf28865750e0f54187d06f18e4b27073c
farid ramy <faridramy2003@gmail.com>
2025-06-30 14:36:17 +0300

    finished all pages frontend with layout and their routes

--- routes/frontend.js
+++ routes/frontend.js
@@ -2,8 +2,14 @@ import express from "express";
 
 const router = express.Router();
 
-router.use("/", (req, res) => {
-  return res.render("index");
-});
+// Route mappings
+router.get("/", (req, res) => res.render("index"));
+router.get("/about-us", (req, res) => res.render("about-us"));
+router.get("/category", (req, res) => res.render("category"));
+router.get("/product-details", (req, res) => res.render("product-details"));
+router.get("/cart", (req, res) => res.render("cart"));
+router.get("/checkout", (req, res) => res.render("checkout"));
+router.get("/contact", (req, res) => res.render("contact"));
+router.get("/account", (req, res) => res.render("account"));
 
 export default router;
